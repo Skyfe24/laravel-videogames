ffc3501fc2f0f30b30a9c3bd740463c2e7418c78
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-09-04 16:52:57 +0200

    errors print on create

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.app')
 @section('content')
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <div class="container mt-5">
         <form action="{{ route('admin.videogames.store') }}" method="POST">
             @csrf
