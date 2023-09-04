4001705d30a997b2a98a591d03bfd3e3004ddbbc
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-09-04 17:01:44 +0200

    alert create fixed

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -1,12 +1,16 @@
 @extends('layouts.app')
 @section('content')
     @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+        <div class="container">
+            <div class="alert alert-danger position-relative">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                    <button type="button" class="m-3 btn-close top-0 end-0 position-absolute" data-bs-dismiss="alert"
+                        aria-label="Close"></button>
+                </ul>
+            </div>
         </div>
     @endif
     <div class="container mt-5">
