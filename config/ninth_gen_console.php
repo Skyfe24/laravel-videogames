d2d5abfec4ea82fafe6c71dc13a038a2fa9600fb
patrictobola <tobola92@gmail.com>
2023-09-08 13:04:15 +0200

    compiled console seeder

---
+++ config/ninth_gen_console.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    ['name' => 'PlayStation 5', 'producer' => 'Sony', 'release_date' => '2020-11-19'],
+    ['name' => 'Xbox Series X', 'producer' => 'Microsoft', 'release_date' => '2020-11-10'],
+    ['name' => 'Xbox Series S', 'producer' => 'Microsoft', 'release_date' => '2020-11-10'],
+    ['name' => 'Steam Deck', 'producer' => 'Valve Corporation', 'release_date' => '2021-07-16']
+];
