2917450f9b5dfe648dc74fe1727373049bcc06d7
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-09-08 13:03:13 +0200

    problem with faker

--- app/Models/Developer.php
+++ app/Models/Developer.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Developer extends Model
 {
     use HasFactory;
+
+    public function developers()
+    {
+        return $this->hasMany(Developer::class);
+    }
 }
--- database/seeders/DeveloperSeeder.php
+++ database/seeders/DeveloperSeeder.php
@@ -2,16 +2,24 @@
 
 namespace Database\Seeders;
 
+use App\Models\Developer;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class DeveloperSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $new_developer = new Developer();
+            $new_developer->name = $faker->words(1, true);
+            $new_developer->surname = $faker->words(1, true);
+            $new_developer->nationality = $faker->words(1, true);
+            $new_developer->save();
+        }
     }
 }
