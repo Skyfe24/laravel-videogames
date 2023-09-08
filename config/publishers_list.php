a5cee30eadbad2ad2097b203d7f38747a01a5e07
patrictobola <tobola92@gmail.com>
2023-09-08 13:21:59 +0200

    compiled publisher seeder and removed faker

---
+++ config/publishers_list.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    ['name' => 'Rockstar Games', 'country' => 'USA'],
+    ['name' => 'Electronic Arts', 'country' => 'USA'],
+    ['name' => 'Ubisoft', 'country' => 'French'],
+    ['name' => 'Capcom', 'country' => 'Japan'],
+    ['name' => 'Bethesda Softworks', 'country' => 'USA'],
+    ['name' => "VisualArt's", 'country' => 'Japan'],
+    ['name' => 'Activision', 'country' => 'USA'],
+    ['name' => 'Paradox Interactive', 'country' => 'Sweden'],
+    ['name' => 'Riot Games', 'country' => 'USA'],
+    ['name' => 'Activision Blizzard', 'country' => 'USA'],
+    ['name' => 'BANDAI NAMCO', 'country' => 'Japan'],
+    ['name' => 'Sega', 'country' => 'Japan'],
+    ['name' => 'Valve', 'country' => 'USA'],
+    ['name' => 'Square Enix', 'country' => 'Japan'],
+];
