bce6d7ce90f4f3a91af86fbde647dde2251d00ff
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-09-08 15:38:47 +0200

    inclyuding genre into videogame seeder

--- database/seeders/VideogameSeeder.php
+++ database/seeders/VideogameSeeder.php
@@ -2,10 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Genre;
 use App\Models\Videogame;
 use Faker\Generator;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
 
 class VideogameSeeder extends Seeder
 {
@@ -14,18 +16,20 @@ class VideogameSeeder extends Seeder
      */
     public function run(Generator $faker): void
     {
+        $genres = Genre::all();
+
         for ($i = 0; $i < 31; $i++) {
 
             $new_videogame = new Videogame();
             $new_videogame->title = $faker->words(3, true);
             $new_videogame->release_date = $faker->dateTime();
-            $new_videogame->genre = $faker->word();
             $new_videogame->cover = $faker->imageUrl(640, 480, 'animals', true);
             $new_videogame->description = $faker->paragraph();
             $new_videogame->serial_number = $faker->isbn13();
             $new_videogame->rating = $faker->numberBetween(1, 5);
             $new_videogame->publisher = $faker->company();
             $new_videogame->save();
+            $new_videogame->genres()->attach(Arr::random($genres->pluck('id')->toArray()));
         }
     }
 }
